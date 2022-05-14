a55e9f20b3432a15abab31e1794fc16c24fd50f5
Laura Jaime <laurajaimev8@gmail.com>
2022-05-14 20:18:55 +0200

    Polyfill for Intersection observer and dynamic imports

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,17 +9,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE}) => {
   const [show, setShow] = useState(false)
 
   useEffect(function () {
-    // Saber si el elemento está en el viewport o no, es decir, si el usuario lo está viendo o no.
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0]
-
-      if (isIntersecting) {
-        console.log('si')
-        setShow(true)
-        observer.disconnect()
-      }
-    })
-    observer.observe(element.current)
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')
+    ).then(() => {
+        // Saber si el elemento está en el viewport o no, es decir, si el usuario lo está viendo o no.
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+    
+          if (isIntersecting) {
+            console.log('si')
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(element.current)
+      })
   }, [element])
 
   return (
