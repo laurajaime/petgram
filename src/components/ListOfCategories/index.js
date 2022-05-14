321e0bc0ed2a5712e2f4f9754a05b3c4148730fc
Laura Jaime <laurajaimev8@gmail.com>
2022-05-14 19:36:40 +0200

    Clean events and show small categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
-
 export const ListOfCategories = () => {
-  console.log(categories)
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-phi-rose.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
