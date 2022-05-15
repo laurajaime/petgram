ebe90118f8156d357860d60379b45458543b8838
Laura Jaime <laurajaimev8@gmail.com>
2022-05-15 12:35:23 +0200

    Extract custom hooks

---
+++ src/hooks/useNearScreen.js
@@ -0,0 +1,26 @@
+import { useEffect, useState, useRef } from 'react'
+
+export function useNearScreen () {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')
+    ).then(() => {
+        // Saber si el elemento está en el viewport o no, es decir, si el usuario lo está viendo o no.
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+    
+          if (isIntersecting) {
+            console.log('si')
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(element.current)
+      })
+  }, [element])
+
+  return [show, element]
+}
