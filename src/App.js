3229ddb16c6cca30c7a1c13e600bb7516be3105d
Laura Jaime <laurajaimev8@gmail.com>
2022-05-21 17:28:26 +0200

    Protected routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
 import { NavBar } from './components/Navbar';
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -21,8 +29,24 @@ export const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/pet/:id' element={<Home />} />
-          <Route path='/detail/:detailId' element={<Detail />} />
+          <Route path='/favs' element={<Favs />} />
+          <Route path='/user' element={<User />} />
         </Routes>
+       
+        <UserLogged>
+          {
+            ({ isAuth }) => 
+              isAuth
+              ? <Routes>
+                <Route path='/favs' element={<Favs />} />
+                <Route path='/user' element={<User />} />
+              </Routes>
+              : <Routes>
+                <Route path='/favs' element={<NotRegisteredUser />} />
+                <Route path='/user' element={<NotRegisteredUser />} />
+              </Routes>
+          }            
+        </UserLogged>
         <NavBar />
       </BrowserRouter>
     </>
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NotRegisteredUser = () => {
+  return <div>NotRegisteredUser</div>;
+};
